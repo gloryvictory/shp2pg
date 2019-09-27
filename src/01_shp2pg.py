db70d6debfa6ca6f8edde2a47d2f21736a88a28b
zamaraev@gmail.com <zamaraev@gmail.com>
2019-09-27 17:13:08 +0500

    01_shp2pg.py - initial commit

--- src/01_shp2pg.py
+++ src/01_shp2pg.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
-#   Author          :   Viacheslav Zamaraev
-#   email           :   zamaraev@gmail.com
+#   Author          : Viacheslav Zamaraev
+#   email           : zamaraev@gmail.com
 #   Script Name     : 01_shp2pg.py
 #   Created         : 25th September 2019
 #   Last Modified	: 25th September 2019
@@ -11,7 +11,9 @@
 # Modifications	: 1.1 -
 #               : 1.2 -
 #
-# Description   : This script will search some *.shp files in the given directory by list (in file_list_shp.txt ) and load to postgresql+postgis
+# Description   : This script will search any *.shp files in the given directory by list (in file_list_shp.txt ) and convert to EPSG:SRID 4326 and load to postgresql+postgis
+# converting by using this utility : ogr2ogr -t_srs EPSG:4326 input_4236.shp input.shp
+
 
 import os                   # Load the Library Module
 import os.path
@@ -22,31 +24,36 @@ from time import strftime   # Load just the strftime Module from Time
 #import logging
 from datetime import datetime
 
-global shpdir
-shpdir = "c:\\test"
-shp2pg_program = "shp2pg"
+global dir_shp_in
+global dir_shp_out
+global program_ogr2ogr
+
+dir_shp_in = "c:\\test"
+dir_shp_out = "c:\\test2"
+program_ogr2ogr = "ogr2ogr"
+#shp2pg_program = "shp2pg"
 
 
-def shp_list_prj(shpdir):
-
+def dir_clear(dir_out =''):
+    if len(str(dir_out)) == 0:
+        dir_out = os.getcwd()
+    filelist = [f for f in os.listdir(dir_out)]
+    for f in filelist:
+        os.remove(os.path.join(dir_out, f))
 
 
 def main():
     time1 = datetime.now()
     print('Starting at :' + str(time1))
 
-    #global LOGGER
-    #LOGGER = InitLogFile()
+    dir_clear(dir_shp_out)
 
-
-    shp_list_prj(shpdir)
-
+    #os.system(program_ogr2ogr + " -t_srs EPSG:4326 " +  file_in +" "+ file_out)
 
     time2 = datetime.now()
     print('Finishing at :' + str(time2))
     print('Total time : ' + str(time2 - time1))
     print('DONE !!!!')
-    #LOGGER.info("Duration scan script: " + str(time2 - time1))
 
 
 if __name__ == '__main__':
