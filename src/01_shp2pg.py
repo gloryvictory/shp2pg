3dfd9ee07636cc52c594336acaea93ff87b7ce7f
zamaraev@gmail.com <zamaraev@gmail.com>
2019-09-27 17:45:41 +0500

    01_shp2pg.py - initial commit

--- src/01_shp2pg.py
+++ src/01_shp2pg.py
@@ -24,6 +24,7 @@ from time import strftime   # Load just the strftime Module from Time
 #import logging
 from datetime import datetime
 
+
 global dir_shp_in
 global dir_shp_out
 global program_ogr2ogr
@@ -42,12 +43,32 @@ def dir_clear(dir_out =''):
         os.remove(os.path.join(dir_out, f))
 
 
+def shp_to_4326(dir_in='', dir_out=''):
+    if len(str(dir_in)) == 0:
+        dir_in = os.getcwd()
+    if len(str(dir_out)) == 0:
+        dir_out = os.getcwd()
+
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(dir_in):
+        for file in f:
+            file_in = str(os.path.join(r, file))
+            ff = file.split(".")[0] + "_4326" + ".shp"
+            file_out = str(os.path.join(dir_shp_out, ff))
+            ext = '.'.join(file.split('.')[1:]).lower()
+            if ext == "shp":
+                #print(os.path.join(r, file))
+                #print(file_in)
+
+                cmd_line = program_ogr2ogr + " -t_srs EPSG:4326 " + file_out + " " + file_in
+                print(cmd_line)
+
 def main():
     time1 = datetime.now()
     print('Starting at :' + str(time1))
 
     dir_clear(dir_shp_out)
-
+    shp_to_4326(dir_shp_in, dir_shp_out)
     #os.system(program_ogr2ogr + " -t_srs EPSG:4326 " +  file_in +" "+ file_out)
 
     time2 = datetime.now()
