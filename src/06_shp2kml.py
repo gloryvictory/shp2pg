12257e378013d3cd387a6d70d2b109fc4fdbfa09
zamaraev@gmail.com <zamaraev@gmail.com>
2019-11-19 12:04:56 +0500

    do some edits 06_shp2kml - init

--- src/06_shp2kml.py
+++ src/06_shp2kml.py
@@ -3,7 +3,7 @@
 #
 #   Author          : Viacheslav Zamaraev
 #   email           : zamaraev@gmail.com
-#   Script Name     : 05_shp2pgsql.py
+#   Script Name     : 06_shp2kml.py
 #   Created         : 30th October 2019
 #   Last Modified	: 30th October 2019
 #   Version		    : 1.0
@@ -11,11 +11,9 @@
 # Modifications	: 1.1 -
 #               : 1.2 -
 #
-# Description   : This script will search any *.shp files in the given directory by list (in file_list_shp.txt ) and convert to EPSG:SRID 4326 and load to postgresql+postgis
-# converting by using this utility : ogr2ogr
-#ogr2ogr  -skipfailures  -overwrite -lco GEOMETRY_NAME=geom -lco LAUNDER=NO -lco precision=NO -a_srs "EPSG:4326" -f "PostgreSQL" PG:"dbname=gisdb host=localhost user=test password=test" /mnt/gisdata/lov_zs.shp -nln "gis.lov_zs"
-#https://kolesovdmitry.github.io/gis_course_win/2/ogr2ogr.html
-#>ogr2ogr -f "PostgreSQL" "PG:host=<hostaddress> user=<user> dbname=<dbname> password=<password>" "C:/shapefile.shp" -nln <schema>.<table>
+# Description   : This script will search any *.shp files in the given directory and convert to KML files
+
+
 
 import os                   # Load the Library Module
 import os.path
@@ -125,38 +123,6 @@ def get_output_directory():
     return dir_out
 
 
-# run sql script through the psql
-def psql_run(host='localhost', dbname='', user='', password='',  sql_statement=''):
-
-    psql_command = '/Applications/Postgres.app/Contents/Versions/10/bin/psql'
-    cmd_line_psql = psql_command + ' '  + '\" dbname=\'' + dbname + '\'' + ' ' \
-                                                                         ' user=\'' + user + '\'' + \
-                    ' password=\'' + password + '\'' + \
-                    ' host=\'' + host + '\'' + '\"' + \
-                    ' -c ' + '\''+ sql_statement + '\''
-    if len(str(sql_statement)) and len(str(host)) and len(str(dbname)) and len(str(user)) and len(str(password)):
-        try:
-            print(cmd_line_psql)
-            logging.info(cmd_line_psql)
-            p = subprocess.Popen(cmd_line_psql, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            for line in p.stdout.readlines():
-                print(line)
-            retval = p.wait()
-
-        except Exception as e:
-            logging.error("Exception occurred", exc_info=True)
-            logging.exception(e)
-            # sys.exit(1)
-            return
-    else:
-        ss = "SQL Statement is NULL. Please specify SQL Statement."
-        print(ss)
-        logging.error(cmd_line_psql)
-        logging.error(sql_statement)
-        logging.error(ss)
-        return
-
-
 def do_shp_dir(dir_input=''):
     program_shp2pgsql = 'ogr2ogr'
     #do log file
@@ -167,14 +133,6 @@ def do_shp_dir(dir_input=''):
         logging.root.removeHandler(handler)
     logging.basicConfig(filename=file_csv, format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG, filemode='w') #
     logging.info(file_csv)
-
-
-    sql1 = 'DROP SCHEMA ' + cfg.schema + ' CASCADE;'
-    psql_run(cfg.host, cfg.database_gis, cfg.user, cfg.user_password, sql1)
-    logging.info(sql1)
-    sql2 = 'CREATE SCHEMA ' + cfg.schema + ';'
-    psql_run(cfg.host, cfg.database_gis, cfg.user, cfg.user_password, sql2)
-    logging.info(sql2)
 
     #do main part
     try:
@@ -189,12 +147,13 @@ def do_shp_dir(dir_input=''):
                     file_name = file_path.split('.')[0]
                     file_prj = file_name + '.prj'
                     file_cp = file_name + '.cpg'
+                    codepage = ''
                     if os.path.isfile(file_prj) and os.path.isfile(file_cp):
                         logging.info(file_path)
 
                         if os.path.isfile(file_cp):
-                            _codepage = get_codepage_from_file(file_cp)
-                            str_err = str(file_cp + ' has codepage ' + _codepage)
+                            codepage = get_codepage_from_file(file_cp)
+                            str_err = str(file_cp + ' has codepage ' + codepage)
                             str_err = str_err.encode("cp1251").decode('cp1251')
                             logging.info(str_err)
                         if os.path.isfile(file_prj):
@@ -209,40 +168,21 @@ def do_shp_dir(dir_input=''):
 
                         if str(_srid) != 'None':
                             srid_source = ' -s ' + str(_srid) + ':4326 '
-                            file_sql = str(os.path.join(dir_out, table_name + '.sql'))
-                            cmd_line = program_shp2pgsql + ' -skipfailures  -overwrite -lco GEOMETRY_NAME=geom -lco LAUNDER=NO -lco precision=NO ' + \
+                            file_kml = str(os.path.join(dir_out, table_name + '.kml'))
+                            cmd_line = program_shp2pgsql + ' -skipfailures  -overwrite  -lco  ENCODING=UTF-8  ' + \
                                 ' -a_srs \"EPSG:4326\"' + \
-                                ' -f ' + '\"PostgreSQL\" PG:\"dbname=' + cfg.database_gis + \
-                                    ' host=' + cfg.host + \
-                                    ' user=' + cfg.user + \
-                                    ' password=' + cfg.user_password + '\"' + \
-                                    ' ' + file_path + \
-                                    ' -nln ' + '\"' + cfg.schema + '.' + table_name + '\"'
+                                ' --config SHAPE_ENCODING ' +       codepage + ' ' +\
+                                ' -f ' + '\"KML\"' + \
+                                ' ' + file_kml + \
+                                ' ' + file_path
+
                             print(cmd_line)
-                            p = subprocess.Popen(cmd_line, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-                            for line in p.stdout.readlines():
-                                print(line)
-                                logging.info(line)
-                            retval = p.wait()
-
-                            # cmd_line_psql = 'psql -d ' + cfg.database_gis + ' -U ' + cfg.user + ' -f ' + file_sql
-                            # psql "dbname='gisdb' user='test' password='test' host='10.57.10.45'" - f / home / glory / 1 / out / газопровод_сводный_2015.sql
-                            #file_sql_run(cfg.host, cfg.database_gis, cfg.user, cfg.user_password, file_sql)
-                            psql_command = '/Applications/Postgres.app/Contents/Versions/10/bin/psql'
-                            cmd_line_psql = psql_command + ' ' + '\" dbname=\'' + cfg.database_gis + '\'' + ' ' \
-                                                                                                 ' user=\'' + cfg.user + '\'' + \
-                                            ' password=\'' + cfg.user_password + '\'' + \
-                                            ' host=\'' + cfg.host + '\'' + '\"' + \
-                                            ' -f ' + file_sql
-                            # if You want run from cmd 'psql .....etc...' line - uncomment please follow lines
-                            # import subprocess
-
-                            print(cmd_line_psql)
-                            # p = subprocess.Popen(cmd_line_psql, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                            # p = subprocess.Popen(cmd_line, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
                             # for line in p.stdout.readlines():
                             #     print(line)
+                            #     logging.info(line)
                             # retval = p.wait()
-                            #
+
 
                     else:
                         logging.error("Filename " + file_prj + ' or ' + file_cp + ' does not exist.')
